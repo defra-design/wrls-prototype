255b77cda72754b08b871e871f4d92e304bac1fb
Chris Barrett <chris.barrett@defra.gov.uk>
2021-04-26 11:56:17 +0100

    Link to monitoring station
    Option to see whether a licence is linked to a monitoring station on the summary table.

--- app/views/sandbox/_routes/_routes.js
+++ app/views/sandbox/_routes/_routes.js
@@ -21,45 +21,12 @@ router.get('/search', function(req, res) {
 
   req.session.data.back = req.headers.referer
 
-
-  //get the search term
-  let term = req.session.data['search']
-
-
-
-  function search(callback) {
-  //Check to see if the results should be a billing account or a licence based on the search term entered
-  //In the proto all billing accounts begin with BA
-
-  if ( term.startsWith("BA") ){
-    req.session.data.resultsTable = "sandbox/_includes/billing-accounts-table.html"
-  } else if ( term.includes("@") ) {
-    req.session.data.resultsTable = "sandbox/_includes/users-table.html"
-  } else if ( term.includes("/") ) {
-    req.session.data.resultsTable = "sandbox/_includes/licences-table.html"
-  } else if ( term.includes("*") ) {
-    req.session.data.resultsTable = "sandbox/_includes/customers-table.html"
-  } else {
-    req.session.data.resultsTable = "sandbox/_includes/stations-table.html"
-  };
-
-
-   callback();
-
-  };
-
-  function render(){
   res.render('sandbox/search');
-};
-
-  search(render);
-
-
 
 });
 
 router.post('/search', function(req, res) {
-
+/*
 //get the search term
 let term = req.session.data['search']
 
@@ -83,6 +50,9 @@ res.redirect('search');
 });
 
 search()
+*/
+
+res.redirect('search');
 
 });
 
