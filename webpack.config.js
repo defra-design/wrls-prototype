6d50e4fbfba831ee9c147d8fdc2b9939b03fdceb
chris-barrett-ddts <chris.barrett@defra.gov.uk>
2023-05-24 09:42:50 +0100

    Update webpack.config.js
    Updating webpack config to use client side config variables

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,12 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   
   plugins: [
-    new Dotenv()
+    new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        OS_API_KEY: JSON.stringify(process.env.OS_API_KEY)
+      }
+    }),
   ],
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   entry: './app/assets/javascripts/map/main.js',
