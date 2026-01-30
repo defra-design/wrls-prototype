a3cb32c4b33f3d2199508683c78fca5636125eac
chris-barrett-ddts <chris.barrett@defra.gov.uk>
2026-01-30 20:43:29 +0000

    Moved to hub and bug fixes
    Lots of bug fixes

--- app/views/sandbox/_routes/_routes-customer.js
+++ app/views/sandbox/_routes/_routes-customer.js
@@ -10,10 +10,13 @@ const folder = "sandbox/customer/"
 //-----------------------------------------
 ///ADD A Contact
 
-
-
 //Contact details
-
+//Create contact hub page
+router.get('/contacts', function(req, res) {
+  req.session.data.success = 0
+  req.session.data.back = req.headers.referer
+  res.render(folder + 'contacts');
+});
 
 //contact type
 router.get('/add/select-a-contact-type', function(req, res) {
@@ -45,6 +48,8 @@ router.get('/add/enter-a-name', function(req, res) {
 });
 
 router.post('/add/enter-a-name', function(req, res) {
+
+  /* Old routing
   let title = req.session.data['title']
   let firstName = req.session.data['first-name']
   let middleInitials = req.session.data['middleInitials']
@@ -59,8 +64,216 @@ router.post('/add/enter-a-name', function(req, res) {
   } else {
     req.session.data.fullName = name
   }
-  res.redirect('enter-an-email-address')
+  res.redirect('enter-an-email-address') */
+
+
+let contacts = req.session.data['contacts']
+  req.session.data.contactID = contacts.length
+
+
+
+  //add the department to the contact list
+  let wrls = "true"
+  let type = req.session.data['contactType']
+  let name = req.session.data['fullName']
+  let title = req.session.data['title']
+  let firstName = req.session.data['first-name']
+  let middleInitials = req.session.data['middleInitials']
+  let lastName = req.session.data['last-name']
+  let suffix = req.session.data['suffix']
+  let department = req.session.data['department']
+  let email = req.session.data['emailDetails']
+  let post = req.session.data['postAddress']
+  let phone = req.session.data['phoneNumber']
+  let role = "Contact"
+  let notices = []
+  let customer = req.session.data.customers[req.session.data.customerID]['name']
+  //let phone = req.session.data['phoneDetails']
+
+  let customers = [{
+    role,
+    notices,
+    customer
+  }]
+  let nameDetails = [{
+    title,
+    firstName,
+    middleInitials,
+    lastName,
+    suffix,
+    department,
+  }]
+
+  let newContact = {
+    name,
+    wrls,
+    type,
+    nameDetails,
+    email,
+    post,
+    phone,
+    customers
+  };
+
+  contacts.push(newContact);
+
+  req.session.data.edit = "false"
+  req.session.data.success = 0
+
+ res.redirect('create-contact')
 });
+
+//Create contact hub page
+router.get('/add/create-contact', function(req, res) {
+  req.session.data.back = req.headers.referer
+  res.render(folder + 'add/create-contact');
+});
+
+router.post('/add/create-contact', function(req, res) {
+
+  req.session.data['contactType'] = ""
+  req.session.data['fullName'] = ""
+  req.session.data['title'] = ""
+  req.session.data['first-name'] = ""
+  req.session.data['middleInitials'] = ""
+  req.session.data['last-name'] = ""
+  req.session.data['suffix'] = ""
+  req.session.data['department'] = ""
+  req.session.data['emailDetails'] = ""
+  req.session.data['postAddress'] = ""
+  req.session.data['phoneNumber'] = ""
+
+  req.session.data.success = "1"
+ if (req.session.data.edit == "true") {
+
+  res.redirect('../manage-contact')
+ } else {
+  
+  res.redirect('../contacts')
+ }
+
+ 
+});
+
+//change email address
+router.get('/add/change-email-address', function(req, res) {
+  req.session.data.back = req.headers.referer
+  res.render(folder + 'add/change-email-address');
+});
+
+router.post('/add/change-email-address', function(req, res) {
+  req.session.data.contacts[req.session.data.contactID].email = req.session.data.emailDetails
+ res.redirect('create-contact')
+});
+
+//change name
+router.get('/add/change-name', function(req, res) {
+  req.session.data.back = req.headers.referer
+  res.render(folder + 'add/change-name');
+});
+
+router.post('/add/change-name', function(req, res) {
+  req.session.data.contacts[req.session.data.contactID].name = req.session.data.fullName
+ res.redirect('create-contact')
+});
+
+
+//change name
+router.get('/add/change-postal-address', function(req, res) {
+  req.session.data.back = req.headers.referer
+  res.render(folder + 'add/change-postal-address');
+});
+
+router.post('/add/change-postal-address', function(req, res) {
+  req.session.data.contacts[req.session.data.contactID].post = req.session.data.postAddress
+ res.redirect('create-contact')
+});
+
+
+//change phone number
+router.get('/add/change-phone-number', function(req, res) {
+  req.session.data.back = req.headers.referer
+  res.render(folder + 'add/change-phone-number');
+});
+
+router.post('/add/change-phone-number', function(req, res) {
+  req.session.data.contacts[req.session.data.contactID].phone = req.session.data.phoneNumber
+ res.redirect('create-contact')
+});
+
+
+router.get('/add/notices', function(req, res) {
+
+  req.session.data.back = req.headers.referer
+
+  req.session.data.alreadyGets = "yes"
+
+  res.render(folder + 'add/notices');
+
+});
+
+router.post('/add/notices', function(req, res) {
+
+let selectedContact = req.session.data.contacts[req.session.data.contactID]
+let selectedCustomer = req.session.data.customers[req.session.data.customerID].name
+let selectedComType = req.session.data.type
+console.log(selectedContact)
+console.log(selectedCustomer)
+
+
+
+ if (req.session.data.notices == "no") {
+
+
+    selectedContact.customers.forEach(customer => {
+      // Check if notices array exists
+      if (customer.notices) {
+        // overwrite the notices array with a version that excludes 'returns'
+        customer.notices = customer.notices.filter(notice => notice.type !== selectedComType );
+      }
+    });
+
+
+   res.redirect('create-contact');
+
+ } else {
+
+// 1. Find or Create the Customer
+let customerIndex = selectedContact.customers.findIndex(c => c.customer === selectedCustomer);
+
+if (customerIndex === -1) {
+  // Create customer if they don't exist
+  selectedContact.customers.push({
+    role: "Contact",
+    notices: [], 
+    customer: selectedCustomer
+  });
+  customerIndex = selectedContact.customers.length - 1;
+}
+
+// 2. Find or Create the Notice inside that customer
+const targetCustomer = selectedContact.customers[customerIndex];
+let noticeIndex = targetCustomer.notices.findIndex(n => n.type === selectedComType);
+
+if (noticeIndex === -1) {
+  // Create notice if it doesn't exist
+  targetCustomer.notices.push({
+    type: selectedComType,
+    sendBy: "email", // Default values
+    addressID: "",
+    licences: "all"
+  });
+  noticeIndex = targetCustomer.notices.length - 1;
+}
+
+
+    res.redirect('all-licences');
+
+  }
+
+  });
+
+
 
 
 //Select how to contact
@@ -113,7 +326,7 @@ router.post('/add/all-licences', function(req, res) {
   if (req.session.data.allLicences == "yes") {
   req.session.data.route = ""
   req.session.data.waaLicences = ""
-  res.redirect('check-your-answers');
+  res.redirect('create-contact');
 } else {
   res.redirect('select-licences');
 }
@@ -129,7 +342,36 @@ router.get('/add/select-licences', function(req, res) {
 });
 
 router.post('/add/select-licences', function(req, res) { 
-   res.redirect('check-your-answers');
+
+    
+
+  //contact customers
+  let contactCustomers = req.session.data.contacts[req.session.data.contactID]['customers']
+  //selected customer
+  let selectedCustomer = req.session.data.customers[req.session.data.customerID]['name']
+  //loop through customers for contact and match the selected customer name if it matches update the notices
+  for (var [customerIndex, contactCustomer] of contactCustomers.entries()) {
+
+    if (contactCustomer.customer == selectedCustomer) {
+
+
+      //get all the contacts notices
+       let contactNotices = req.session.data.contacts[req.session.data.contactID].customers[customerIndex]['notices']
+       //loop through the contacts notices
+       for (var [contactNoticeIndex, contactNotice] of contactNotices.entries()) {
+
+         //check for the notice that equals type water abstraction alerts
+            if (contactNotice.type == req.session.data.type ) {
+
+              // Set the WAA licences for this contact-
+              contactNotice.licences = req.session.data['waaLicences']
+
+            }
+          }
+        }
+      }
+
+   res.redirect('create-contact');
 
 });
 
@@ -271,11 +513,59 @@ router.get('/add/contact-set-up', function(req, res) {
 });
 
 
+/////////////Cancel set up
+
+//Are you sure/check your answers
+router.get('/cancel/check-your-answers', function(req, res) {
+
+  req.session.data.back = req.headers.referer
+
+  res.render(folder + '/cancel/check-your-answers');
+
+});
+
+
+router.post('/cancel/check-your-answers', function(req, res) {
+
+   //Remove the contact
+   //get the ID of the contact that you want to remove
+     let contactID = req.session.data['contactID']
+
+     //get the contacts List
+     let contacts = req.session.data['contacts']
+
+     //set the removed contacts name to so we can use it in the success banner after it has been removed
+     req.session.data.removedName = req.session.data.contacts[contactID]['name']
+
+     //remove the object that is in the position set by the contact ID
+     if (contactID > -1) {
+       contacts.splice(contactID, 1);
+     }
+
+  req.session.data['contactType'] = ""
+  req.session.data['fullName'] = ""
+  req.session.data['title'] = ""
+  req.session.data['first-name'] = ""
+  req.session.data['middleInitials'] = ""
+  req.session.data['last-name'] = ""
+  req.session.data['suffix'] = ""
+  req.session.data['department'] = ""
+  req.session.data['emailDetails'] = ""
+  req.session.data['postAddress'] = ""
+  req.session.data['phoneNumber'] = ""
+
+    res.redirect('../contacts');
+
+});
+
+
 ///////////////////////////////////////////////////
 //Manage customer contacts
 router.get('/manage-contact', function(req, res) {
 
   req.session.data.back = req.headers.referer
+
+  req.session.data.success= "0"
 
   res.render(folder + 'manage-contact');
 
@@ -535,7 +825,7 @@ router.post('/remove/check-your-answers', function(req, res) {
        contacts.splice(contactID, 1);
      }
 
-    res.redirect('contact-removed');
+    res.redirect('../contacts');
 
 });
 
